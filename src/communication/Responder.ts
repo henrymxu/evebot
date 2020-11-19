d4b0da354318b2829bfb9947c1bb0fdac2b781c9
Kevin Shen <kevin_shen@hotmail.ca>
2020-11-19 17:01:10 -0500

    Cleanup acknowledge emojis (#3)
    * changed conditional to object
    
    * Update constant name

--- src/communication/Responder.ts
+++ src/communication/Responder.ts
@@ -3,6 +3,11 @@ import {Message, MessageEmbed, MessageOptions, TextChannel} from "discord.js"
 import {Communicator} from "./Communicator"
 import {MessageGenerator} from "./MessageGenerator"
 
+const DEFAULT_EMOJIS = {
+    0: '👌',
+    1: '👎',
+};
+
 export default class Responder {
     private context: GuildContext
     private messageCache: Map<string, Message[]> = new Map()
@@ -20,13 +25,7 @@ export default class Responder {
         if (!message) {
             return
         }
-        let emoji: string
-        if (mode == 0) {
-            emoji = '👌'
-        } else {
-            emoji = '👎'
-        }
-        Communicator.acknowledge(emoji, message)
+        Communicator.acknowledge(DEFAULT_EMOJIS[mode], message)
     }
 
     send(message: BotMessage, removeAfter?: number): Promise<Message[]> {
