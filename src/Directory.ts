c3303fb697bd53a052ab88eda22585ce722e013f
Henry Xu <henry.m.xu@live.com>
2020-12-29 15:36:38 -0500

    Add check to determine if in build folder

--- src/Directory.ts
+++ src/Directory.ts
@@ -2,9 +2,10 @@ import * as path from 'path';
 
 export namespace Directory {
     export const ROOT_DIR = __dirname;
+    export const RELATIVE_ROOT_DIR = determineRoot(ROOT_DIR);
 
     export function relativeRoot(file: string): string {
-        return path.join(ROOT_DIR, `../${file}`);
+        return path.join(RELATIVE_ROOT_DIR, `../${file}`);
     }
 
     export function relativeSource(file: string): string {
@@ -12,10 +13,19 @@ export namespace Directory {
     }
 
     export function relativeConfig(file: string): string {
-        return path.join(ROOT_DIR, `../configs/${file}`);
+        return path.join(RELATIVE_ROOT_DIR, `../configs/${file}`);
     }
 
     export function relativeResources(file: string): string {
-        return path.join(ROOT_DIR, `../resources/${file}`);
+        return path.join(RELATIVE_ROOT_DIR, `../resources/${file}`);
+    }
+}
+
+function determineRoot(dir: string): string {
+    const parsed = path.parse(path.join(dir, '../'));
+    if (parsed.base === 'build') {
+        return path.join(dir, '../');
+    } else {
+        return dir;
     }
 }
